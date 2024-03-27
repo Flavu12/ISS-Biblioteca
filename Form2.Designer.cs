f1e5c54a9a9d6b43299abd9ad0ba39dbf02cc8fd
Flavia Busnatu <155023953+Flavu12@users.noreply.github.com>
2024-03-27 18:45:51 +0200

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Iss_Lab1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(40, 98);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(207, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(40, 153);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(207, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(40, 217);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(207, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(40, 279);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(207, 67);
+            richTextBox1.TabIndex = 3;
+            richTextBox1.Text = "";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(40, 386);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(207, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(456, 98);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(210, 23);
+            textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(456, 153);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(210, 23);
+            textBox6.TabIndex = 6;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(495, 217);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 49);
+            button1.TabIndex = 7;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(463, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 15);
+            label1.TabIndex = 8;
+            label1.Text = "username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(463, 135);
+            label2.Name = "label2";
+            label2.Size = new Size(57, 15);
+            label2.TabIndex = 9;
+            label2.Text = "password";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(40, 79);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 15);
+            label3.TabIndex = 10;
+            label3.Text = "FirstName";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(40, 135);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 15);
+            label4.TabIndex = 11;
+            label4.Text = "LastName";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(40, 199);
+            label5.Name = "label5";
+            label5.Size = new Size(31, 15);
+            label5.TabIndex = 12;
+            label5.Text = "CNP";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(40, 261);
+            label6.Name = "label6";
+            label6.Size = new Size(42, 15);
+            label6.TabIndex = 13;
+            label6.Text = "Adress";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(40, 368);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 15);
+            label7.TabIndex = 14;
+            label7.Text = "PhoneNumber";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private RichTextBox richTextBox1;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+    }
+}
---
+++ Form5.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Iss_Lab1
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listaCartiDisponibile = new ListBox();
+            Logout = new Button();
+            Imprumuta = new Button();
+            Returneaza = new Button();
+            listaCartiInProcesDeReturnare = new ListBox();
+            SuspendLayout();
+            // 
+            // listaCartiDisponibile
+            // 
+            listaCartiDisponibile.FormattingEnabled = true;
+            listaCartiDisponibile.ItemHeight = 15;
+            listaCartiDisponibile.Location = new Point(29, 53);
+            listaCartiDisponibile.Name = "listaCartiDisponibile";
+            listaCartiDisponibile.Size = new Size(288, 349);
+            listaCartiDisponibile.TabIndex = 0;
+            // 
+            // Logout
+            // 
+            Logout.Location = new Point(814, 484);
+            Logout.Name = "Logout";
+            Logout.Size = new Size(136, 45);
+            Logout.TabIndex = 1;
+            Logout.Text = "Logout";
+            Logout.UseVisualStyleBackColor = true;
+            // 
+            // Imprumuta
+            // 
+            Imprumuta.Location = new Point(88, 425);
+            Imprumuta.Name = "Imprumuta";
+            Imprumuta.Size = new Size(174, 40);
+            Imprumuta.TabIndex = 2;
+            Imprumuta.Text = "Imprumuta";
+            Imprumuta.UseVisualStyleBackColor = true;
+            // 
+            // Returneaza
+            // 
+            Returneaza.Location = new Point(528, 427);
+            Returneaza.Name = "Returneaza";
+            Returneaza.Size = new Size(192, 38);
+            Returneaza.TabIndex = 3;
+            Returneaza.Text = "Returneaza";
+            Returneaza.UseVisualStyleBackColor = true;
+            // 
+            // listaCartiInProcesDeReturnare
+            // 
+            listaCartiInProcesDeReturnare.FormattingEnabled = true;
+            listaCartiInProcesDeReturnare.ItemHeight = 15;
+            listaCartiInProcesDeReturnare.Location = new Point(449, 53);
+            listaCartiInProcesDeReturnare.Name = "listaCartiInProcesDeReturnare";
+            listaCartiInProcesDeReturnare.Size = new Size(321, 349);
+            listaCartiInProcesDeReturnare.TabIndex = 4;
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(968, 548);
+            Controls.Add(listaCartiInProcesDeReturnare);
+            Controls.Add(Returneaza);
+            Controls.Add(Imprumuta);
+            Controls.Add(Logout);
+            Controls.Add(listaCartiDisponibile);
+            Name = "Form5";
+            Text = "Form5";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox listaCartiDisponibile;
+        private Button Logout;
+        private Button Imprumuta;
+        private Button Returneaza;
+        private ListBox listaCartiInProcesDeReturnare;
+    }
+}
